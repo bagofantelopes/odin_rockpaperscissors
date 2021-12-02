45d582c9d500358a23a5ca9d212ef19675b37cb3
Travis F <agathangalos@gmail.com>
2021-12-02 18:36:01 -0500

    Made some minor tweaks to some string values that are displayed in the divs. Added and cleaned up comments in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Global variables to track wins and hold the possible moves in the game
-const moves = ['rock', 'paper', 'scissors']
+// Global variables to track scores and wins and such
 let roundCounter = 0;
 let maxRounds = 5;
+let tieCounter = 0;
 
 const playerWinCounter = document.getElementById('player-score');
 const computerWinCounter = document.getElementById('computer-score');
@@ -9,22 +9,29 @@ const currentRound = document.getElementById('current-round');
 const finalScore = document.getElementById('final-score');
 
 
-// Function to decide the computer's move with a simple random number generator
+// Decide the computer's move with a simple random number generator
+// based on a string array of the possible moves in RockPaperScissors
 const computerPlay = () => {
+    const moves = ['rock', 'paper', 'scissors']
     let num = Math.floor(Math.random() * (moves.length - 0) + 0);
     return moves[num];
 }
 
+// should be setting the maxRounds variable based on user input
+// but is presently fubar
 const selectRounds = () => {
     maxRounds = document.getElementById('max-rounds-selector');
     console.log(maxRounds.target.value);
 }
 
-/*
-Function to run each round of the game. Generates the moves for player and computer
-based on function calls and runs those variable through the gameLogic function. 
-Prints results of gameLogic() to console.
-*/
+// 'main' function
+// increments the roundCounter with each run (starts at 1 ofc)
+// generates the computer's move with computerPlay() 
+// and displays it alongside the player's move in the appropriate divs
+// calls gameLogic() function to decide winner of each round.
+// once roundCounter has reached the set amount, displays the final 
+// scores and the winner of the game in a new div
+
 const playRound = (player) => {
     roundCounter++;
     
@@ -38,29 +45,31 @@ const playRound = (player) => {
     gameLogic(player.toLowerCase(), computer);
 
     if (roundCounter >= maxRounds) {
-        finalScore.innerText = `You have won ${playerWinCounter.value} rounds and the computer has won ${computerWinCounter.value} rounds. `
+        finalScore.innerText = `You have won ${playerWinCounter.value} rounds and the computer has won ${computerWinCounter.value} rounds. \n `+
+                                `Also, ${tieCounter} rounds ended in a tie. `;
         if (playerWinCounter.value > computerWinCounter.value) {
             finalScore.appendChild(document.createTextNode("You have won the game!"))
         } else if (computerWinCounter.value > playerWinCounter.value) {
             finalScore.appendChild(document.createTextNode("The computer has won the game!"))
-            console.log("The computer has won the game!")
         } else {
-            finalScore.appendChild(document.createTextNode("Looks like a tie this time!"))
-            console.log("Looks like a tie this time!")
+            finalScore.appendChild(document.createTextNode("Looks like this game was a tie!"))
         }
     }
 }
 
-/* 
-Function to handle the obnoxious conditional tree. 
-(I think a switch statement might technically be better here but I hate them)
-Also increments the global win counter for player and computer.
-*/
+
+// Function to handle the underlying game logic of RockPaperScissors
+// Reads the textContent of the appropriate divs to get player/computer moves
+// compares the strings with a simple conditional tree
+// each round's winner is decided in the conditionals, and the appropriate
+// score counter is incremented
+
 const gameLogic = (player, computer) => {
     console.log('player: ' + player);
     console.log('computer: ' + computer);
     currentRound.style.display = 'block';
     if (player == computer) {
+        tieCounter++;
         currentRound.textContent = `Round ${roundCounter} Winner: IT'S A TIE`;
     } else if (player == 'rock' && computer == 'paper') {
         computerWinCounter.value++;
@@ -84,6 +93,9 @@ const gameLogic = (player, computer) => {
 
 }
 
+
+// Event handler for the process for a user selecting how many
+// rounds they want to play. currently fubar and wip
 let startBtn = document.getElementById('start-button');
 //startBtn.addEventListener('click', selectRounds);
 startBtn.addEventListener('click', (e) => {
@@ -91,17 +103,20 @@ startBtn.addEventListener('click', (e) => {
 })
 
 // player move button event listeners
+// decides the player's move based on which button they click
 const buttons = document.querySelectorAll('#move-container');
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-        //console.log(e.target.textContent);
         playRound(e.target.textContent);
     });
 });
 
 const reset = document.getElementById('reset-button');
 
+
+// 'Play Again?' button. Resets everything to its default state
+// so another game can be played.
 reset.addEventListener('click', () => {
     roundCounter = 0;
     computerWinCounter.value = 0;
