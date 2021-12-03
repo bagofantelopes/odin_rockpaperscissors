ea888d9ab38b55ee3790f7fca6b63f031c93c522
Travis F <agathangalos@gmail.com>
2021-12-02 23:37:55 -0500

    All features now functional, and styling mostly done. A lot of work can be done to clean this up and add pizazz but is is about finished.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Global variables to track scores and wins and such
 let roundCounter = 0;
 let maxRounds = 5;
-let tieCounter = 0;
 
 const playerWinCounter = document.getElementById('player-score');
 const computerWinCounter = document.getElementById('computer-score');
@@ -17,47 +16,6 @@ const computerPlay = () => {
     return moves[num];
 }
 
-// should be setting the maxRounds variable based on user input
-// but is presently fubar
-const selectRounds = () => {
-    maxRounds = document.getElementById('max-rounds-selector');
-    console.log(maxRounds.target.value);
-}
-
-// 'main' function
-// increments the roundCounter with each run (starts at 1 ofc)
-// generates the computer's move with computerPlay() 
-// and displays it alongside the player's move in the appropriate divs
-// calls gameLogic() function to decide winner of each round.
-// once roundCounter has reached the set amount, displays the final 
-// scores and the winner of the game in a new div
-
-const playRound = (player) => {
-    roundCounter++;
-    
-    computer = computerPlay();
-
-    playerMove = document.getElementById('your-move');
-    computerMove = document.getElementById('computer-move');
-    playerMove.textContent = `You play: ${player.toLowerCase()}`
-    computerMove.textContent = `Computer Plays: ${computer}`
-
-    gameLogic(player.toLowerCase(), computer);
-
-    if (roundCounter >= maxRounds) {
-        finalScore.innerText = `You have won ${playerWinCounter.value} rounds and the computer has won ${computerWinCounter.value} rounds. \n `+
-                                `Also, ${tieCounter} rounds ended in a tie. `;
-        if (playerWinCounter.value > computerWinCounter.value) {
-            finalScore.appendChild(document.createTextNode("You have won the game!"))
-        } else if (computerWinCounter.value > playerWinCounter.value) {
-            finalScore.appendChild(document.createTextNode("The computer has won the game!"))
-        } else {
-            finalScore.appendChild(document.createTextNode("Looks like this game was a tie!"))
-        }
-    }
-}
-
-
 // Function to handle the underlying game logic of RockPaperScissors
 // Reads the textContent of the appropriate divs to get player/computer moves
 // compares the strings with a simple conditional tree
@@ -65,12 +23,11 @@ const playRound = (player) => {
 // score counter is incremented
 
 const gameLogic = (player, computer) => {
-    console.log('player: ' + player);
-    console.log('computer: ' + computer);
+
     currentRound.style.display = 'block';
     if (player == computer) {
-        tieCounter++;
         currentRound.textContent = `Round ${roundCounter} Winner: IT'S A TIE`;
+        return;
     } else if (player == 'rock' && computer == 'paper') {
         computerWinCounter.value++;
         currentRound.textContent = `Round ${roundCounter} Winner: THE COMPUTER!`;
@@ -93,34 +50,73 @@ const gameLogic = (player, computer) => {
 
 }
 
+// 'main' function
+// increments the roundCounter with each run (starts at 1 ofc)
+// generates the computer's move with computerPlay() 
+// and displays it alongside the player's move in the appropriate divs
+// calls gameLogic() function to decide winner of each round.
+// once roundCounter has reached the set amount, displays the final 
+// scores and the winner of the game in a new div
 
-// Event handler for the process for a user selecting how many
-// rounds they want to play. currently fubar and wip
-let startBtn = document.getElementById('start-button');
-//startBtn.addEventListener('click', selectRounds);
-startBtn.addEventListener('click', (e) => {
-    console.log(e.target.value);
-})
+const playRound = (player) => {
+
+    document.getElementById('playing-for-rounds').textContent = `Playing till ${maxRounds} points!`
+    roundCounter++;
+    
+    computer = computerPlay();
+    console.log(player);
+    playerMove = document.getElementById('your-move');
+    computerMove = document.getElementById('computer-move');
+    playerMove.textContent = `YOU'VE CHOSEN: ${player.toUpperCase()}`
+    computerMove.textContent = `THEY'VE CHOSEN: ${computer.toUpperCase()}`
+
+    gameLogic(player, computer);
+    console.log(playerWinCounter.value);
+
+    if (playerWinCounter.value == maxRounds)
+        finalScore.appendChild(document.createTextNode("You have won the game!"))
+    if (computerWinCounter.value == maxRounds) 
+        finalScore.appendChild(document.createTextNode("The computer has won the game!"))
+};
+
+// Event handler for the process for a user to set how many
+// points they need to win
+const startBtn = document.getElementById('start-button');
+startBtn.addEventListener('click', () => {
+    maxRounds = document.getElementById('max-rounds-selector').value;
+});
 
 // player move button event listeners
 // decides the player's move based on which button they click
-const buttons = document.querySelectorAll('#move-container');
-
-buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        playRound(e.target.textContent);
-    });
+const rock = document.getElementById('rock');
+rock.addEventListener('click', () => {
+    playRound('rock');
 });
 
-const reset = document.getElementById('reset-button');
+const paper = document.getElementById('paper');
+paper.addEventListener('click', () => {
+    playRound('paper');
+});
 
+const scissors = document.getElementById('scissors');
+scissors.addEventListener('click', () => {
+    playRound('scissors');
+});
 
 // 'Play Again?' button. Resets everything to its default state
 // so another game can be played.
+const reset = document.getElementById('reset-button');
+
 reset.addEventListener('click', () => {
     roundCounter = 0;
     computerWinCounter.value = 0;
     playerWinCounter.value = 0;
     finalScore.innerText = "";
-    currentRound.style.display = "hidden";
-})
+    currentRound.style.display = "none";
+    document.getElementById('your-move').textContent = "";
+    document.getElementById('computer-move').textContent = "";
+    document.getElementById('playing-for-rounds').textContent = "";
+    document.getElementById('max-rounds-selector').value = 5;
+});
+
+
